dbbbce13e9209a81bbc3009173dbe546749e925d
seiya128 <s.watanabe@illustrious.co.jp>
2017-06-20 22:52:17 +0900

    忘れ物

--- db/migrate/20170620130445_create_tweets.rb
+++ db/migrate/20170620130445_create_tweets.rb
@@ -1,6 +1,6 @@
 class CreateTweets < ActiveRecord::Migration[5.1]
 	def change
-		create_table :tweets do |t|
+		create_table :tweets, :options => 'ENGINE=InnoDB CHARSET=utf8' do |t|
 			t.text :contents
 			t.column :created_at, 'DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP'
 			t.column :updated_at, 'TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'
