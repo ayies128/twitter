b4984ca3e7021f6d0086f6d53d660a731faa2220
seiya128 <s.watanabe@illustrious.co.jp>
2017-06-20 22:49:29 +0900

    忘れ物

--- db/migrate/20170620130445_create_tweets.rb
+++ db/migrate/20170620130445_create_tweets.rb
@@ -2,7 +2,8 @@ class CreateTweets < ActiveRecord::Migration[5.1]
 	def change
 		create_table :tweets do |t|
 			t.text :contents
-			t.timestamps
+			t.column :created_at, 'DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP'
+			t.column :updated_at, 'TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'
 		end
 	end
 end
