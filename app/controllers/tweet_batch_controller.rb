775d0a2dd5f737548e66e5a9220cae3579dcb172
seiya128 <s.watanabe@illustrious.co.jp>
2017-10-28 17:43:04 +0900

    PAAPIががばすぎるので対策

--- app/controllers/tweet_batch_controller.rb
+++ app/controllers/tweet_batch_controller.rb
@@ -1,24 +1,36 @@
 class TweetBatchController < ApplicationController
 
 	def afiri
-		begin
-			# Amazon Product Advertising API へのアクセスに必要なキーを設定
-			paapi_account = PaapiAccount.find(1)
-			Amazon::Ecs.configure do |options|
-				options[:associate_tag] = paapi_account.associate_tag
-				options[:AWS_access_key_id] = paapi_account.access_key_id
-				options[:AWS_secret_key] = paapi_account.secret_key
+		# Amazon Product Advertising API へのアクセスに必要なキーを設定
+		paapi_account = PaapiAccount.find(1)
+		Amazon::Ecs.configure do |options|
+			options[:associate_tag] = paapi_account.associate_tag
+			options[:AWS_access_key_id] = paapi_account.access_key_id
+			options[:AWS_secret_key] = paapi_account.secret_key
+		end
+
+		loop_count = 1
+		loop do
+			begin
+				res = Amazon::Ecs.item_search(
+					SearchWord.all.shuffle.first.word, 
+					{
+						search_index: AmazonSearchIndex.all.shuffle.first.name, 
+						response_group: 'ItemAttributes,Offers',
+						item_page: 1, 
+						country: 'jp', 
+						sort: 'salesrank'
+					}
+				)
+				break
+			rescue => e
+				loop_count += 1
+				break if loop_count > 10
+				sleep(11)
+				next
 			end
-			res = Amazon::Ecs.item_search(
-				SearchWord.all.shuffle.first.word, 
-				{
-					search_index: AmazonSearchIndex.all.shuffle.first.name, 
-					response_group: 'ItemAttributes,Offers',
-					item_page: 1, 
-					country: 'jp', 
-					sort: 'salesrank'
-				}
-			)
+		end
+		if res.present?
 			item = res.items.shuffle.first
 			title = item.get('ItemAttributes/Title')
 			manufacturer = item.get('ItemAttributes/Manufacturer')
@@ -29,7 +41,7 @@ class TweetBatchController < ApplicationController
 			else
 				tweet = Tweet.all.shuffle.first.contents
 			end
-		rescue => e
+		else
 			tweet = Tweet.all.shuffle.first.contents
 		end
 		@twitter.update(tweet)
